1ac3ea990be66460efb7a2c208a946f074e2e538
Nick Schot <nickschot@gmail.com>
2019-01-05 00:42:54 +0100

    implement initial version of ember-mobile-pane integration

--- addon/components/mobile-calendar.js
+++ addon/components/mobile-calendar.js
@@ -20,7 +20,34 @@ export default Component.extend({
       : null;
   }),
 
+  _center: computed('_value', 'center', function(){
+    if(this.get('center')){
+      return moment(this.get('center'));
+    } else if(moment.isMoment(this.get('_value'))){
+      return this.get('_value');
+    } else {
+      return moment();
+    }
+  }),
+
+  previousCenter: computed('_center', function(){
+    const date = this.get('_center').clone().subtract(1, 'month');
+
+    return { date }
+  }),
+  nextCenter: computed('_center', function(){
+    const date = this.get('_center').clone().add(1, 'month');
+
+    return { date }
+  }),
+
   actions: {
+    changeCenter(targetIndex, targetModel){
+      if(targetModel && get(targetModel, 'date')){
+        this.set('center', get(targetModel, 'date'));
+      }
+    },
+
     select(closeAction, day){
       closeAction();
       this.get('onSelect')(day ? get(day, 'date') : null);
